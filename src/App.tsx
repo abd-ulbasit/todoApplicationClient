d352ead24f093776642e8d5c5f0e24727bc9b762
Abdul Basit <basit01035@gmail.com>
2022-09-17 15:06:43 +0500

    "Initilized the project"

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='bg-slate-600 font-extrabold'>
+      hello
+    </div>
+  )
+}
+
+export default App
